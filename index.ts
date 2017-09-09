bab4df4a71371465444745505a4f1c42474f44a9
minami <sourcechord@gmail.com>
2017-09-09 09:37:54 +0900

    Update packages
    * upgrade monaco-editor v0.10.0
    * upgrade electron v1.7.5
    * upgrade typescript v2.5.2

--- index.ts
+++ index.ts
@@ -5,8 +5,15 @@ const app = remote.app;
 const BrowserWindow = remote.BrowserWindow;
 const dialog = remote.dialog;
 
+declare var amdRequire;
 var editor: monaco.editor.IStandaloneCodeEditor;
 
+// loader.jsのrequire関数を使い、monaco-editorのコードを読み込む
+amdRequire(['vs/editor/editor.main'], () => {
+  onModuleLoaded();
+});
+
+// monaco-editorのモジュール読み込み完了時に行う、エディタ初期化処理
 function onModuleLoaded() {
     editor = monaco.editor.create(document.getElementById('container'), {
         value: [
@@ -24,3 +31,6 @@ function showEditorText() {
     var text = editor.getValue();
     alert(text);
 }
+
+
+document.getElementById('btnShowText').onclick = showEditorText;
